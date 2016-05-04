c296e5f1db052de87ffc84eb9131af9a38c3a53a
elcasa123 <dcasanov@alumnos.ubiobio.cl>
2016-05-04 11:35:45 -0300

    primer commit

---
+++ src/java/test/cl/ubb/agil/pilaTest.java
@@ -0,0 +1,14 @@
+package cl.ubb.agil;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pilaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
